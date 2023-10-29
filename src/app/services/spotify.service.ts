2bd429c1fbe9bddf55e5e54a98529bb609dd0656
Gustavo De Almeida Bassanetto <gustavo.almeidabassanetto@gmail.com>
2023-10-29 00:22:48 -0300

    Spotify :: Correção dos erros relacionados as requisições de usuário e playlist

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import {SpotifyConfiguration} from "../../environments/environment";
+import { Router } from '@angular/router';
 import Spotify from 'spotify-web-api-js';
-import { IUsuario } from '../Interfaces/IUsuario';
+import { SpotifyConfiguration } from 'src/environments/environment';
+import { SpotifyPlaylistParaPlaylist, SpotifyUserParaUsuario } from '../Common/spotifyHelper';
 import { IPlaylist } from '../Interfaces/IPlaylist';
-import { SpotifyUserParaUsuario } from '../Common/spotifyHelper'
-import { SpotifyPlaylistParaPlaylist } from '../Common/spotifyHelper'
+import { IUsuario } from '../Interfaces/IUsuario';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,7 @@ export class SpotifyService {
   spotifyApi: Spotify.SpotifyWebApiJs = null;
   usuario: IUsuario;
 
-  constructor() {
+  constructor(private router: Router) {
     this.spotifyApi = new Spotify();
   }
 
@@ -25,61 +25,47 @@ export class SpotifyService {
     const token = localStorage.getItem('token');
 
     if(!token)
-      return false
+      return false;
 
     try {
-
       this.definirAccessToken(token);
       await this.obterSpotifyUsuario();
       return !!this.usuario;
 
-    } catch (ex) {
+    }catch(ex){
       return false;
     }
   }
 
-  // async obterSpotifyUsuario() {
-  //   const userInfo = await this.spotifyApi.getMe();
-  //   this.usuario = SpotifyUserParaUsuario(userInfo);
-  // }
-
   async obterSpotifyUsuario() {
-    try {
-      const userInfo = await this.spotifyApi.getMe();
-      console.log('Resposta do Spotify API:', userInfo);
-      this.usuario = SpotifyUserParaUsuario(userInfo);
-    } catch (error) {
-      console.error('Erro ao obter informações do usuário:', error);
-    }
+    const userInfo = await this.spotifyApi.getMe();
+    this.usuario = SpotifyUserParaUsuario(userInfo);
   }
 
-
   obterUrlLogin() {
-    const authEndPoint = `${SpotifyConfiguration.authEndPoint}?`;
+    const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
-    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
+    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`;
     const responseType = `response_type=token&show_dialog=true`;
-    return authEndPoint + clientId + redirectUrl + scopes + responseType;
+    return authEndpoint + clientId + redirectUrl + scopes + responseType;
   }
 
   obterTokenUrlCallback() {
     if (!window.location.hash)
       return '';
 
-    const params = window.location.hash.substring(1).split('&')
+    const params = window.location.hash.substring(1).split('&');
     return params[0].split('=')[1];
   }
 
-  definirAccessToken(token: string) {
+  definirAccessToken(token: string){
     this.spotifyApi.setAccessToken(token);
     localStorage.setItem('token', token);
   }
 
-  async buscarPlaylistUsuario(offset = 0, limit = 50) :Promise<IPlaylist[]>{
-    console.log(this.usuario);
+  async buscarPlaylistUsuario(offset = 0, limit = 50): Promise<IPlaylist[]>{
     const playlists = await this.spotifyApi.getUserPlaylists(this.usuario.id, { offset, limit });
-    console.log(playlists);
     return playlists.items.map(SpotifyPlaylistParaPlaylist);
   }
 }
