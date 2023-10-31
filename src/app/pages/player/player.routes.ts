87e27938533e1f0613b8d76937604564b2130c32
GustavoObara <luizobara@gmail.com>
2023-10-31 16:20:30 -0300

    component dashboard sendo criado e rota realizada

--- src/app/pages/player/player.routes.ts
+++ src/app/pages/player/player.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { PlayerComponent } from "./player.component"
 import { HomeComponent } from "../home/home.component";
 import { ListaMusicasComponent } from "../lista-musicas/lista-musicas.component";
+import { DashboardComponent } from "../dashboard/dashboard.component";
 
 export const PlayerRotas: Routes = [
     {
@@ -11,6 +12,10 @@ export const PlayerRotas: Routes = [
             {
                 path: 'home',
                 component: HomeComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
             },
             {
                 path: 'lista/:tipo/:id',
